60f7103f3fe57c58b66a694e3f2fd488531869bf
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-20 17:29:36 +0200

    k

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,3 +1,13 @@
-export const schema = {
-  types: [],
-}
+export const schemaTypes = [
+  {
+    name: 'perfume',
+    title: 'العطور 🧴',
+    type: 'document',
+    fields: [
+      { name: 'name', title: 'اسم العطر', type: 'string' },
+      { name: 'price', title: 'السعر', type: 'number' },
+      { name: 'description', title: 'وصف العطر', type: 'text' },
+      { name: 'image', title: 'صورة العطر', type: 'image', options: { hotspot: true } },
+    ]
+  }
+]
