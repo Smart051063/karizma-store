d19011dabd79f9cd8f50eb5e952f1154cab2b700
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-20 19:38:01 +0200

    k

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: 'qdlep10i', // معرف مشروعك Karizma-Store
+  projectId: 'qdlep10i', // رقم مشروعك الجديد
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: false, // اجعلها false لترى التحديثات فوراً أثناء التطوير
+  useCdn: false, // اجعلها false لترى التحديثات فوراً بدون تأخير الكاش
 })
