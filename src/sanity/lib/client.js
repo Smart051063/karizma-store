f16ce419c603fe1f34dce2ab262e82474b0ae07a
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-21 11:41:38 +0200

    j

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId: 'qdlep10i', // 👈 تأكد أن هذا هو الرقم الجديد
+  projectId: 'qdlep10i',
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: false, // 👈 اجعلها false (مهم جداً لكي تظهر التعديلات فوراً)
-})npm run dev
+  useCdn: false,
+})
