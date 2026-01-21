bb75033f414eda7bb67ede71f490cfb595439c67
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-21 14:27:20 +0200

    ش

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+'use client'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
 
-export default eslintConfig;
+// استيراد السكيما مع تحديد المسار الكامل لتجنب الأخطاء
+// (نضيف /index.js لضمان العثور على الملف)
+import {schema} from './src/sanity/schemaTypes/index.js'
+
+// تعريف المتغيرات مباشرة هنا بدلاً من استيرادها من ملف خارجي
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+
+export default defineConfig({
+  basePath: '/studio',
+  projectId,
+  dataset,
+  schema,
+  plugins: [
+    structureTool(),
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
