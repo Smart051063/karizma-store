cd07ec28a06833aa0e2030093ffb9e806fcedc4b
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-21 17:38:13 +0200

    a

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,8 +1,5 @@
 import { blockContentType } from './blockContentType'
-import { categoryType } from './categoryType' // (إذا كان لديك ملف للتصنيفات، وإلا احذف هذا السطر)
-import { postType } from './postType' // (نفس الشيء، احذف السطر إذا لم يكن لديك مدونة)
-import product from './product' // 👈 هذا هو السطر الأهم! استيراد ملف المنتج الجديد
+import product from './product'
 
-export const schema = {
-  types: [product], // 👈 ونضعه هنا في القائمة
-}
+// لاحظ أننا غيرنا الاسم هنا ليصبح schemaTypes ليتوافق مع ملف الإعدادات
+export const schemaTypes = [blockContentType, product]
