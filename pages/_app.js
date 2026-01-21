f1f14cb2d19a543d5e63b76081859ab037e383e2
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-21 16:10:16 +0200

    ض

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
 import Navbar from '../components/Navbar';
-import TopBar from '../components/TopBar'; // استيراد الشريط الجديد
+import TopBar from '../components/TopBar';
+import Footer from '../components/Footer'; // 1. استيراد الفوتر
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
-      <TopBar /> {/* الشريط التنبيهي في الأعلى */}
-      <Navbar /> {/* شريط التنقل تحته */}
-      <Component {...pageProps} />
+      <TopBar />   {/* في الأعلى: شريط التنبيه */}
+      <Navbar />   {/* تحته: القائمة */}
+      
+      <div style={{ minHeight: '80vh' }}> {/* حيلة صغيرة لضمان عدم صعود الفوتر للأعلى إذا كانت الصفحة فارغة */}
+        <Component {...pageProps} /> {/* هنا يظهر محتوى الصفحات (المنتجات، السلة، إلخ) */}
+      </div>
+
+      <Footer />   {/* 2. في الأسفل: التذييل */}
     </CartProvider>
   );
 }
