5da67fb3fce7ac13d590c5ff0019dd7f370b34e3
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-21 15:05:59 +0200

    q

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { CartProvider } from '../context/CartContext';
+import Navbar from '../components/Navbar'; // 1. استدعاء النافبار
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      <Navbar /> {/* 2. وضع النافبار هنا ليظهر في كل الصفحات */}
       <Component {...pageProps} />
     </CartProvider>
   );
