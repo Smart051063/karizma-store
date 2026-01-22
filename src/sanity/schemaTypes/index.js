20ae67b5de0e06fc30ec0aabc9cd188815cc7318
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-23 00:51:10 +0200

    z

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,5 +1,6 @@
-import { product } from './product'
-import banner from './banner' // تأكد أنك أنشأت ملف banner.js بجانبه
+// لاحظ: حذفنا الأقواس من حول كلمة product
+import product from './product' 
+import banner from './banner'
 
 export const schema = {
   types: [product, banner],
