b9ec3eafbb756fc1410d8e7be98a7fce9f1e5891
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-25 19:56:31 +0200

    r

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,2 +1,21 @@
-NEXT_PUBLIC_SANITY_PROJECT_ID="qdlep10i"
-NEXT_PUBLIC_SANITY_DATASET="production"
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01'
+
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+)
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+)
+
+export const useCdn = false
+
+function assertValue(v, errorMessage) {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+  return v
+}
