9a1563957a49a8631d115d63b350e84553e2d268
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-26 12:38:44 +0200

    ق

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,44 @@
-import React, { useState, useEffect } from 'react'; // 👈 تأكد من استيراد useState و useEffect
-// ... باقي الاستيرادات ...
+import React, { useState, useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
+// 👇 تأكد من وجود هذين السطرين
+import { CartProvider } from '../src/context/CartContext';
+import { LanguageProvider } from '../src/context/LanguageContext'; 
+import Script from 'next/script';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  const [mounted, setMounted] = useState(false); // 👈 متغير الحالة
+  // حالة للتأكد من تحميل الصفحة بالكامل (لحل مشكلة Hydration)
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true); // 👈 تفعيله عند التحميل
+    setMounted(true);
   }, []);
 
   return (
     <LanguageProvider>
       <CartProvider>
-        {/* ... أكواد السكربت ... */}
+        
+        {/* كود تيك توك بيكسل */}
+        <Script
+          id="tiktok-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+                var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+                ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
 
-        {/* 👇 اعرض التوستر فقط عندما يكون mounted = true */}
+                ttq.load('D5R89ABC77U6BSHUH0G0');
+                ttq.page();
+              }(window, document, 'ttq');
+            `,
+          }}
+        />
+
+        {/* عرض التوستر فقط بعد التحميل */}
         {mounted && <Toaster />}
         
         <Navbar /> 
@@ -24,4 +49,5 @@ function MyApp({ Component, pageProps }) {
     </LanguageProvider>
   )
 }
+
 export default MyApp;
