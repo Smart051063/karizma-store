085162daa0a53cead7d534781c4a413f4a767b69
khaled elsebay <smart.repair.cairo@gmail.com>
2026-01-31 16:31:44 +0200

    ش

--- pages/studio/[[...index]].js
+++ pages/studio/[[...index]].js
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import { NextStudio } from 'next-sanity/studio'
-// نفترض أن ملف الإعدادات موجود في المجلد الرئيسي (خارج src أو pages)
+// 👇 هذا السطر يفترض أن ملف sanity.config.js موجود في المجلد الرئيسي للمشروع
+// إذا ظهر خطأ في المسار، تأكد أن عدد النقاط (../../) يوصل للملف الصحيح
 import config from '../../sanity.config' 
 
 export default function StudioPage() {
   return (
     <>
       <Head>
-        {/* إعدادات بسيطة للعنوان والعرض لضمان عمل الاستوديو بشكل جيد على الجوال */}
         <title>لوحة التحكم | Karizma Studio</title>
         <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
-        <meta name="robots" content="noindex" /> {/* لمنع جوجل من أرشفة لوحة التحكم */}
+        <meta name="robots" content="noindex" /> {/* يمنع جوجل من أرشفة لوحة الأدمن */}
       </Head>
       
-      {/* تشغيل الاستوديو */}
+      {/* تشغيل الاستوديو بالإعدادات المستوردة */}
       <NextStudio config={config} />
     </>
   )
