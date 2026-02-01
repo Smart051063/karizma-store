8fc9391989de94cbd4ea509385ef843a6f79472b
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-01 10:18:14 +0200

    r

---
+++ next-sitemap.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.karizmaperfumes.com', // رابط موقعك
+  generateRobotsTxt: true, // لإنشاء ملف robots.txt
+  sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/studio', '/api'], // حماية لوحة التحكم
+      },
+    ],
+    additionalSitemaps: [
+      'https://www.karizmaperfumes.com/sitemap.xml',
+    ],
+  },
+}
