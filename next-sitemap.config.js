cf1c1967f19386c72dd9eb9db8cb07c6737e404f
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-01 13:09:55 +0200

    Fix sitemap loop

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.karizmaperfumes.com', // رابط موقعك
-  generateRobotsTxt: true, // لإنشاء ملف robots.txt
+  siteUrl: 'https://www.karizmaperfumes.com',
+  generateRobotsTxt: true,
   sitemapSize: 7000,
+  
+  // 👇 حذفنا قسم additionalSitemaps لأنه هو سبب المشكلة
+  
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/studio', '/api'], // حماية لوحة التحكم
+        disallow: ['/studio', '/api'],
       },
     ],
-    additionalSitemaps: [
-      'https://www.karizmaperfumes.com/sitemap.xml',
-    ],
   },
-} 
+}
