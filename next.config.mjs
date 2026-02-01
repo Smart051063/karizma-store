221ce4282c5b72769b32ba313876a0ca39a48e9e
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-01 09:11:46 +0200

    ق

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default nextConfig;
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.karizmaperfumes.com', // رابط موقعك
+  generateRobotsTxt: true, // ✅ هذا السطر هو الذي سينشئ ملف robots.txt
+  sitemapSize: 7000,
+  
+  // التحكم في الملف
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/', // اسمح لجوجل بدخول كل الموقع
+        disallow: ['/studio', '/api'], // ⛔ امنع جوجل من دخول لوحة التحكم والـ API
+      },
+    ],
+    // إضافة رابط الخريطة داخل الملف
+    additionalSitemaps: [
+      'https://www.karizmaperfumes.com/sitemap.xml',
+    ],
+  },
+}
