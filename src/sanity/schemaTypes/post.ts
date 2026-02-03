8495a66d9480bc193927f52ccdd55b4af5a6534f
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-03 12:28:09 +0200

    ق

---
+++ src/sanity/schemaTypes/post.ts
@@ -0,0 +1,43 @@
+export default {
+  name: 'post',
+  title: 'Blog Post (المقالات)',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'عنوان المقال',
+      type: 'string',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'رابط المقال (Slug)',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'mainImage',
+      title: 'صورة الغلاف',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'publishedAt',
+      title: 'تاريخ النشر',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+    },
+    {
+      name: 'body',
+      title: 'محتوى المقال',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+  ],
+}
