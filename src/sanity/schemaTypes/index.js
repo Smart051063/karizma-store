d0955308d1218deb4a098e7dffd49a04921c6e0c
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-03 12:31:41 +0200

    ق

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,6 +1,9 @@
-// 👇 لاحظ: استيراد بدون أقواس (لأننا نستخدم export default في الملفات)
+// 👇 استيراد الملف الجديد
+import post from './post'
+
+// 👇 استيراد ملفاتك القديمة كما هي
 import product from './product'
 import banner from './banner'
 
-// 👇 لاحظ: غيرنا الاسم إلى schemaTypes ليرضى ملف الإعدادات
-export const schemaTypes = [product, banner]
+// 👇 دمج الجميع في القائمة النهائية
+export const schemaTypes = [product, banner, post]
