65f3f5a78a7505faa2c5fe0fba64d2afbe6044b8
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-04 12:44:39 +0200

    ش

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,8 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      }
+      { protocol: 'https', hostname: 'cdn.sanity.io' },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
     ],
   },
   typescript: {
