7ff9572472aec569ac0ce901f378cffb1d84f642
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-09 13:53:00 +0200

    r

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,10 +1,9 @@
-import { product } from './product'
-import { banner } from './banner'
-import { post } from './post'
-// 👇 1. استدعاء الملف الجديد
-import landingPage from './landingPage' 
+// ✅ الاستيراد الصحيح (بدون أقواس)
+import product from './product'
+import banner from './banner'
+import post from './post'
+import landingPage from './landingPage'
 
 export const schema = {
-  // 👇 2. إضافته للقائمة
-  types: [product, banner, post, landingPage], 
+  types: [product, banner, post, landingPage],
 }
