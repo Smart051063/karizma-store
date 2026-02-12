f50442ff142c7a811aa9842ab82d7d963b327cb8
khaled elsebay <smart.repair.cairo@gmail.com>
2026-02-12 09:36:42 +0200

    ق

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -4,5 +4,16 @@ import banner from './banner'
 import post from './post'
 import landingPage from './landingPage'
 
+// 👇 1. استدعاء الملفات الجديدة هنا
+import customer from './customer'
+import review from './review'
+
 // ✅ الحل: تصدير المصفوفة باسم schemaTypes ليقبلها ملف الإعدادات
-export const schemaTypes = [product, banner, post, landingPage]
+export const schemaTypes = [
+  product, 
+  banner, 
+  post, 
+  landingPage, 
+  customer, // 👈 2. تمت الإضافة هنا
+  review    // 👈 وتمت الإضافة هنا
+]
